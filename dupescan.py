39c75ee927694a030fc1fe8f171d9ad0c8e9b286
pavelromanovski <fake-email@dontbotherme.com>
2013-02-08 19:03:57 -0500

    updated command line help menu

--- dupescan.py
+++ dupescan.py
@@ -2,6 +2,7 @@
 import os
 import hashlib
 import sys
+import argparse
 
 '''
 Peeks around a specified folder and its subfolders checking if any duplicate files exist.
@@ -11,6 +12,11 @@ Whether a file is considered a duplicate depends on its contents, not its filena
 location = r"."
 database = {}
 VERBOSE = False
+# contains arguments passed from command line or their defaults in their absence
+cmdArgs = None
+
+def printVerbose(text):
+    pass
 
 def read_dirs():
     "Reads all files from a given location"
@@ -31,6 +37,13 @@ def read_dirs():
         if VERBOSE == True: print()
 
 if __name__ == "__main__":
+    argsParser = argparse.ArgumentParser()
+    argsParser.add_argument('-v', '--verbose', help='verbose mode', action='store_true')
+    argsParser.add_argument('-a', '--all', help='scans all files including hidden files and folders', action='store_true')
+    argsParser.add_argument('location', help='file location of where to begin search', default=r'.', nargs='?')
+
+    cmdArgs = argsParser.parse_args()
+
     if len(sys.argv) > 1:
         location = sys.argv[1]
     print("Checking if any duplicate files exist under \"{0}\".".format(location))
